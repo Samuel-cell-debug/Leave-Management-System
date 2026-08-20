6ef809b170200f59dabad68fbe8d800928341d45
Samuel-cell-debug <successrichman@gmail.com>
2026-08-20 11:53:41 +0000

    config: finalize Apps Script deployment URL
    - Update apiUrl with the deployed Web App endpoint (script.google.com/.../exec)
    - Ensure the URL points to the new version of Code.gs with fixed doGet/doPost
    - Ready for production testing of login and leave request flows

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,4 +1,4 @@
 window.NORTHSTAR_SHEETS = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbz2OmFG7GJz761PVDIjT9q8A4AyQu4d1CH-6r78UE4_zcAGTW_bXjTUU2vTM74A1B4U0g/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwgflZEs4eqwJe-LV-Uv30AM32_aQ6BQpmlEki53ICuZpD4ThH9hpbx_q1H8pKhP4drPw/exec",
   spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1WaDhn0hSf-UtnKK-SucBkht5Cw0PA14e0Qvyaz5LTDQ/edit?gid=0#gid=0"
 };
