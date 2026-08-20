21be79357e7ec704d546f5da47a5d53125e6d4c0
Samuel-cell-debug <successrichman@gmail.com>
2026-08-20 11:06:19 +0000

    config: add spreadsheet URL and prepare for Apps Script deployment
    - Set spreadsheetUrl with the Google Sheet link for direct access
    - Leave apiUrl empty until the Apps Script Web App deployment is complete
    - Ensure proper separation between data source and API endpoint

--- google-sheets-config.js
+++ google-sheets-config.js
@@ -1,3 +1,4 @@
 window.NORTHSTAR_SHEETS = {
-  apiUrl: ""
+  apiUrl: "https://script.google.com/macros/s/AKfycbz2OmFG7GJz761PVDIjT9q8A4AyQu4d1CH-6r78UE4_zcAGTW_bXjTUU2vTM74A1B4U0g/exec",
+  spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1WaDhn0hSf-UtnKK-SucBkht5Cw0PA14e0Qvyaz5LTDQ/edit?gid=0#gid=0"
 };
